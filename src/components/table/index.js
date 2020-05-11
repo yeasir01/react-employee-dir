60e1cff90c8093c5c5aa70169c6fa63da91a3c6f
Yeasir H <yeasir01@gmail.com>
2020-05-11 00:49:47 -0700

    got api call to work

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,44 +1,59 @@
-import API from '../../utils/api';
-import React from 'react';
+import React, {Component} from 'react';
+import Axios from 'axios';
+import './style.css'
 
-const Table = () => {
-    return (
-        <div className="row">
-            <div className="col">
-                <table className="table">
-                    {console.log(JSON.stringify(API.getUsers()))}
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>Some</td>
-                        </tr>
-                    </tbody>
-                </table>
+
+class Table extends Component {
+    state = {
+        data: [],
+        count: 0,
+        isLoading: true
+    };
+
+
+    componentDidMount() {
+        Axios
+        .get('https://randomuser.me/api/?seed=employee&results=30&nat=us')
+        .then(res => {
+            this.setState({
+            data: res.data.results, 
+            count: res.data.results.length
+            })
+        })
+    };
+
+    render(){
+        return (
+            <div className="row">
+                <div className="col">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col" className="text-secondary">First</th>
+                                <th scope="col" className="text-secondary">Last</th>
+                                <th scope="col" className="text-secondary">Email</th>
+                                <th scope="col" className="text-secondary">Cell</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.data.map(emp => {
+                                    return(
+                                        <tr className="cursor">
+                                            <th scope="row"><img id="thumbnail" src={emp.picture.thumbnail} alt="employee pic"/></th>
+                                            <td className="align-middle">{emp.name.first}</td>
+                                            <td className="align-middle">{emp.name.last}</td>
+                                            <td className="align-middle">{emp.email}</td>
+                                            <td className="align-middle">{emp.cell}</td>
+                                        </tr>
+                                    )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 };
 
 export default Table;
