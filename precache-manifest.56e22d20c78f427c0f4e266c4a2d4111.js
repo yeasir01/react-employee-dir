656d1071ef08490f63417af7e6c410c87f7edfcf
Yeasir H <yeasir01@gmail.com>
2020-05-14 07:09:43 -0700

    Updates

---
+++ precache-manifest.56e22d20c78f427c0f4e266c4a2d4111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137af03576832989839cbb510a1d6bde",
+    "url": "/react-employee-dir/index.html"
+  },
+  {
+    "revision": "c719083baccb6a266bd7",
+    "url": "/react-employee-dir/static/css/main.2e7ee33d.chunk.css"
+  },
+  {
+    "revision": "9a7f6869815379649056",
+    "url": "/react-employee-dir/static/js/2.a9140bc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-dir/static/js/2.a9140bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c719083baccb6a266bd7",
+    "url": "/react-employee-dir/static/js/main.1fa923d5.chunk.js"
+  },
+  {
+    "revision": "18d0ad6e36bf18e13741",
+    "url": "/react-employee-dir/static/js/runtime-main.5bd0974d.js"
+  },
+  {
+    "revision": "d6cd5151c04765d1992edfde14483068",
+    "url": "/react-employee-dir/static/media/load.d6cd5151.gif"
+  }
+]);
