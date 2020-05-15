ac9abe2917d887a7eb2f8435d8990d746fe2f988
Yeasir H <yeasir01@gmail.com>
2020-05-14 21:56:52 -0700

    corrected syntax error class to className

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div class="d-flex justify-content-center">
+        <div className="d-flex justify-content-center">
             <div className="spinner-border text-primary " style={{width: "3rem", height: "3rem"}} role="status">
                 <span className="sr-only">Loading...</span>
             </div>
